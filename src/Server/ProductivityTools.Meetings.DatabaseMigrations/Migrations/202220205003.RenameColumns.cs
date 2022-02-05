8186ee7c1b12a40d32056d313bf859b437fe447d
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-02-05 14:11:59 +0100

    rename columns to old

---
+++ src/Server/ProductivityTools.Meetings.DatabaseMigrations/Migrations/202220205003.RenameColumns.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.Meetings.DatabaseMigrations.Migrations
+{
+    [Migration(20220205003)]
+    public class RenameColumns: Migration
+    {
+        public override void Down()
+        {
+        }
+
+        public override void Up()
+        {
+            Rename.Column("BeforeNotes").OnTable("JournalItem").InSchema("jl").To("BeforeNotes_old");
+            Rename.Column("AfterNotes").OnTable("JournalItem").InSchema("jl").To("AfterNotes_old");
+            Rename.Column("DuringNotes").OnTable("JournalItem").InSchema("jl").To("DuringNotes_old");
+        }
+    }
+}
