09b62fc85ac7e8f0bc6c3e8583a996868391bf13
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-04-21 07:04:40 +0200

    details type column

---
+++ src/Server/ProductivityTools.Journal.DatabaseMigrations/Migrations/20220425001.DetailsType.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.Meetings.DatabaseMigrations.Migrations
+{
+    [Migration(20220425001)]
+    public class AddDetailsType: Migration
+    {
+        public override void Down()
+        {
+        }
+
+        public override void Up()
+        {
+            Alter.Table("JournalItemNotes")
+                     .InSchema("jl").AddColumn("NotesType").AsString(8).Nullable();
+        }
+    }
+}
