6cd115086df1bbabe59e06443fb72fc6b34c2edc
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-05-05 22:55:08 +0200

    grzes

--- src/Server/ProductivityTools.Journal.WebApi/AuthenticatedUsers.cs
+++ src/Server/ProductivityTools.Journal.WebApi/AuthenticatedUsers.cs
@@ -18,7 +18,7 @@ namespace ProductivityTools.Journal.WebApi
             IEnumerable<Claim> claims = identity.Claims;
             var email = claims.First(x => x.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress").Value;
 
-            if (email == "pwujczyk@gmail.com" || email=="pwujczyk@google.com")
+            if (email == "pwujczyk@gmail.com" || email=="pwujczyk@google.com" || email == "g.opara@gmail.com")
             {
                 base.OnActionExecuting(context);
             }
