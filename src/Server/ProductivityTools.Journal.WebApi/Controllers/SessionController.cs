8c478365fa46dfcdcac5ee7562991c7c174b23bb
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-08-06 14:13:26 +0200

    login

--- src/Server/ProductivityTools.Journal.WebApi/Controllers/SessionController.cs
+++ src/Server/ProductivityTools.Journal.WebApi/Controllers/SessionController.cs
@@ -19,68 +19,68 @@ namespace ProductivityTools.Journal.WebApi.Controllers
             return View();
         }
 
-        [HttpPost]
-        [Route("Login")]
-        public async Task<ActionResult> Login([FromBody] LoginRequest request)
-        {
-            // Set session expiration to 5 days.
-            var options = new SessionCookieOptions()
-            {
-                ExpiresIn = TimeSpan.FromDays(5),
-            };
+        //[HttpPost]
+        //[Route("Login")]
+        //public async Task<ActionResult> Login([FromBody] LoginRequest request)
+        //{
+        //    // Set session expiration to 5 days.
+        //    var options = new SessionCookieOptions()
+        //    {
+        //        ExpiresIn = TimeSpan.FromDays(5),
+        //    };
 
-            try
-            {
-                var cookieOptions = new CookieOptions();
-                cookieOptions.Expires = DateTime.Now.AddDays(1);
-                cookieOptions.SameSite = SameSiteMode.None;
-                //cookieOptions.Path = "/";
-                cookieOptions.Secure = true;
-                cookieOptions.HttpOnly = true;
-                Response.Cookies.Append("fsdafa", "Fsad", cookieOptions);
-                //Response.Cookies.Append("Bearer", request.IdToken, cookieOptions);
-                Response.Headers.Add("Access-Control-Allow-Credentials", "true");
-                Response.Headers.Add("access-control-expose-headers", "Set-Cookie");
-                Response.Headers.Add("Access-Control-Allow-Origin", "https://localhost:3000");
-                return this.Ok();
-            }
-            catch (FirebaseAuthException)
-            {
-                return this.Unauthorized("Failed to create a session cookie");
-            }
-        }
+        //    try
+        //    {
+        //        var cookieOptions = new CookieOptions();
+        //        cookieOptions.Expires = DateTime.Now.AddDays(1);
+        //        cookieOptions.SameSite = SameSiteMode.None;
+        //        //cookieOptions.Path = "/";
+        //        cookieOptions.Secure = true;
+        //        cookieOptions.HttpOnly = true;
+        //        Response.Cookies.Append("fsdafa", "Fsad", cookieOptions);
+        //        //Response.Cookies.Append("Bearer", request.IdToken, cookieOptions);
+        //        Response.Headers.Add("Access-Control-Allow-Credentials", "true");
+        //        Response.Headers.Add("access-control-expose-headers", "Set-Cookie");
+        //        Response.Headers.Add("Access-Control-Allow-Origin", "https://localhost:3000");
+        //        return this.Ok();
+        //    }
+        //    catch (FirebaseAuthException)
+        //    {
+        //        return this.Unauthorized("Failed to create a session cookie");
+        //    }
+        //}
 
-        [HttpGet]
-        [Route("LoginGet")]
-        public async Task<ActionResult> LoginGet(string token)
-        {
-            //var bearer = Request.Headers.Authorization[0].Replace("Bearer ", "");
+        //[HttpGet]
+        //[Route("LoginGet")]
+        //public async Task<ActionResult> LoginGet(string token)
+        //{
+        //    //var bearer = Request.Headers.Authorization[0].Replace("Bearer ", "");
 
-            // Set session expiration to 5 days.
-            var options = new SessionCookieOptions()
-            {
-                ExpiresIn = TimeSpan.FromDays(5),
-            };
+        //    // Set session expiration to 5 days.
+        //    var options = new SessionCookieOptions()
+        //    {
+        //        ExpiresIn = TimeSpan.FromDays(5),
+        //    };
 
-            try
-            {
-                var cookieOptions = new CookieOptions();
-                cookieOptions.Expires = DateTime.Now.AddDays(1);
-                cookieOptions.SameSite = SameSiteMode.None;
-                //cookieOptions.Path = "/";
-                cookieOptions.Secure = true;
-                cookieOptions.HttpOnly = true;
-                Response.Cookies.Append("token", token, cookieOptions);
-                //Response.Cookies.Append("Bearer", request.IdToken, cookieOptions);
-                Response.Headers.Add("Access-Control-Allow-Credentials", "true");
-                //Response.Headers.Add("access-control-expose-headers", "Set-Cookie");
-                Response.Headers.Add("Access-Control-Allow-Origin", "https://localhost:3000");
-                return this.Ok();
-            }
-            catch (FirebaseAuthException)
-            {
-                return this.Unauthorized("Failed to create a session cookie");
-            }
-        }
+        //    try
+        //    {
+        //        var cookieOptions = new CookieOptions();
+        //        cookieOptions.Expires = DateTime.Now.AddDays(1);
+        //        cookieOptions.SameSite = SameSiteMode.None;
+        //        //cookieOptions.Path = "/";
+        //        cookieOptions.Secure = true;
+        //        cookieOptions.HttpOnly = true;
+        //        Response.Cookies.Append("token", token, cookieOptions);
+        //        //Response.Cookies.Append("Bearer", request.IdToken, cookieOptions);
+        //        Response.Headers.Add("Access-Control-Allow-Credentials", "true");
+        //        //Response.Headers.Add("access-control-expose-headers", "Set-Cookie");
+        //        Response.Headers.Add("Access-Control-Allow-Origin", "https://localhost:3000");
+        //        return this.Ok();
+        //    }
+        //    catch (FirebaseAuthException)
+        //    {
+        //        return this.Unauthorized("Failed to create a session cookie");
+        //    }
+        //}
     }
 }
