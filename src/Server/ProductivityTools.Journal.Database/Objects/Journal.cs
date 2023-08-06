f890221afc3d105f9ceb1090632b739a7e075951
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-08-06 15:28:54 +0200

    parent

--- src/Server/ProductivityTools.Journal.Database/Objects/Journal.cs
+++ src/Server/ProductivityTools.Journal.Database/Objects/Journal.cs
@@ -11,7 +11,5 @@ namespace ProductivityTools.Meetings.Database.Objects
         public int? ParentId { get; set; }
         public string Name { get; set; }
         public bool Deleted { get; set; }
-        public Journal Parent { get; set; }
-
     }
 }
