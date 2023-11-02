3d0659dc19d0f81865ecc3bd68b6b052feffcc10
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-11-02 21:39:57 +0100

    echo

--- src/Server/ProductivityTools.Journal.WebApi/Controllers/MeetingsController.cs
+++ src/Server/ProductivityTools.Journal.WebApi/Controllers/MeetingsController.cs
@@ -61,6 +61,13 @@ namespace ProductivityTools.Meetings.WebApi.Controllers
             return "pawel2";
         }
 
+        [HttpGet]
+        [Route("echo")]
+        public string echo(string name)
+        {
+            return $"Welcome request performed at {DateTime.Now} with param {name} on server {System.Environment.MachineName} to Application Jounral";
+        }
+
         [HttpPost]
         [Route("Date")]
         public object GetDate()
